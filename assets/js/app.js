0f37a1a3c1d820cbc72f7e8f8cc11ae8bb15b044
karljohan122 <karljohan122@gmail.com>
2024-09-08 20:37:17 +0300

    Adding line numbers feature EP.10

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,9 +23,34 @@ import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+
+let hooks = {}
+
+hooks.updateLineNumbers = { 
+  mounted() {
+    this.el.addEventListener("input", () => {
+      this.updateLineNumbers()
+    })
+
+    this.updateLineNumbers()
+  },
+
+  updateLineNumbers() {
+    const lineNumberText = document.querySelector("#line-numbers")
+    if (!lineNumberText) return
+
+    const lines = this.el.value.split("\n")
+
+    const numbers = lines.map((_, index) => index + 1).join("\n") + "\n"
+
+    lineNumberText.value = numbers
+  }
+}
+ 
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken}
+  params: {_csrf_token: csrfToken},
+  hooks: hooks
 })
 
 // Show progress bar on live navigation and form submits
